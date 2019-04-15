4699671e9470c820431ed31aacd92f716058cc19
riizdo <ricardoizquierdopro@gmail.com>
2019-04-15 21:45:59 +0200

    añadido el termometro y sus imagenes

--- termometro.py
+++ termometro.py
@@ -8,6 +8,34 @@ class Termometro():
         self.__custome = pygame.image.load("imagenes/termometro.png")
         
         
+    def imagen(self):
+        return self.__custome
+    
+        
+        
+class Selector():
+    def __init__(self, unidad = 'C'):
+        self.__unidad = unidad
+        self.__custome = {}
+        
+        self.__custome['C'] = pygame.image.load("imagenes/selectorC.png")
+        self.__custome['F'] = pygame.image.load("imagenes/selectorF.png")
+        
+        
+    def imagen(self, valor = None):
+        if valor == None:
+            return self.__custome[self.__unidad]
+        elif valor == 'C' or valor == 'F':
+            return self.__custome[valor]
+        
+        
+    def evento(self, event):
+        if self.__unidad == 'C':
+            self.__unidad = 'F'
+        elif self.__unidad == 'F':
+            self.__unidad = 'C'
+        
+        
         
 class NumberInput():
     __value = 0
@@ -35,12 +63,12 @@ class NumberInput():
         rect.left = self.__position[0]
         rect.top = self.__position[1]
         rect.size = self.__size
-        ...
-        return {
-            'fondo' = rect,
-            'texto' = textBlock
-            }
-        ...
+        
+        #return {
+        #    'fondo' = rect,
+        #    'texto' = textBlock
+        #    }
+        
         
         return (rect, textBlock)
     
@@ -133,6 +161,8 @@ class MainApp():
         self.__entrada = NumberInput()
         self.__entrada.pos((106, 58))
         self.__entrada.size((133, 28))
+        
+        self.__selector = Selector()
             
             
     def on_close(self):
@@ -146,13 +176,22 @@ class MainApp():
                 if event.type == pygame.QUIT:
                     self.on_close()
                     
-                if event.type == KEYDOWN:
+                elif event.type == pygame.KEYDOWN:
                     self.__entrada.evento(event)
+                    
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.__selector.evento(event)
+                    
+            self.__screen.fill((244, 236, 203))
     
-            self.__screen.blit(self.__termometro.custome(), (50, 34))
-            text = self.__entrada.reder()
+            self.__screen.blit(self.__termometro.imagen(), (50, 34))
+            
+            text = self.__entrada.render()
             pygame.draw.rect(self.__screen, (255, 255, 255), text[0])
             self.__screen.blit(text[1], self.__entrada.pos())
+            
+            self.__screen.blit(self.__selector.imagen(), (112, 153))
+            
             pygame.display.flip()
         
 
